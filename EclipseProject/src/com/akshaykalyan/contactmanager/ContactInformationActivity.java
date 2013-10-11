e038991e6b2faba3112bc066605206ca2ebc5b58
Akshay Kalyan <akshaykalyan@outlook.com>
2013-10-11 22:11:26 +1300

    Fixed bug where user can press back into contact info after deletion as activity was placed on stack

--- EclipseProject/src/com/akshaykalyan/contactmanager/ContactInformationActivity.java
+++ EclipseProject/src/com/akshaykalyan/contactmanager/ContactInformationActivity.java
@@ -331,6 +331,7 @@ public class ContactInformationActivity extends Activity {
 				        Intent intent = new Intent(getActivity().getApplicationContext(), ContactListActivity.class);
 				        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     	startActivity(intent);
+                    	getActivity().finish();
 				        
 				        
 				        
